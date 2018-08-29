8619051e506ecbaaf6740b387546408b43de42af
Sollace <sollacea@gmail.com>
2018-08-29 15:16:14 +0200

    Document target

--- src/common/java/com/minelittlepony/jumpingcastle/api/Target.java
+++ src/common/java/com/minelittlepony/jumpingcastle/api/Target.java
@@ -1,8 +1,21 @@
 package com.minelittlepony.jumpingcastle.api;
 
+/**
+ * Targetting method to control who recieves a message when sent.
+ * Senders choose whether a message is recieved on other clients, just the server, or both.
+ */
 public enum Target {
+    /**
+     * Message is ignored by the server and forwarded to connected clients.
+     */
     CLIENTS(true, false),
+    /**
+     * Only the server should process this message. Clients ignore it.
+     */
     SERVER(false, true),
+    /**
+     * The server should process this message and forward it to all available clients.
+     */
     SERVER_AND_CLIENTS(true, true);
 
     private final boolean clients;
@@ -14,10 +27,16 @@ public enum Target {
         this.servers = servers;
     }
 
+    /**
+     * True if messages with this targetting must be procesed by the client.
+     */
     public boolean isClients() {
         return clients;
     }
 
+    /**
+     * True if messages with this targetting must be procesed by the server.
+     */
     public boolean isServers() {
         return servers;
     }
